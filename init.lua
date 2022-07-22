0552f8f9f774dee4d8366a829b6cde83734784b2
F4isalSH <f4isalcode@gmail.com>
2022-07-22 22:04:25 +0300

    added COC

--- init.lua
+++ init.lua
@@ -16,28 +16,19 @@ return require("packer").startup(function()
 		-- or                            , branch = '0.1.x',
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
-	use({
-		"williamboman/nvim-lsp-installer",
-		"neovim/nvim-lspconfig",
-	})
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-vsnip")
-	use("hrsh7th/vim-vsnip")
-	use("rafamadriz/friendly-snippets")
+	use({ "neoclide/coc.nvim", branch = "release" })
 
 	--Configurations
 	vim.opt.termguicolors = true
 	vim.wo.number = true
+	vim.cmd("source $HOME/.config/nvim/coc_config.vim")	
 	local formatting = require("null-ls").builtins.formatting
 	local sources = {
 		formatting.prettier,
 		formatting.eslint,
 		formatting.stylua,
 	}
+
 	require("null-ls").setup({
 		sources = sources,
 		on_attach = function(client, bufnr)
@@ -71,42 +62,6 @@ return require("packer").startup(function()
 		"javascript",
 		html = {
 			mode = "foreground",
-		},
-	})
-
-	local cmp = require("cmp")
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			end,
-		},
-		window = {},
-
-		mapping = cmp.mapping.preset.insert({
-			["<C-k>"] = cmp.mapping.select_prev_item(),
-			["<C-j>"] = cmp.mapping.select_next_item(),
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		}),
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-			{ name = "vsnip" }, -- For vsnip users.
-		}, {
-			{ name = "buffer" },
-		}),
-	})
-	require("nvim-lsp-installer").setup({
-		automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-		ui = {
-			icons = {
-				server_installed = "✓",
-				server_pending = "➜",
-				server_uninstalled = "✗",
-			},
 		},
 	})
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,35 +74,10 @@ _G.packer_plugins = {
     path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
     url = "https://github.com/akinsho/bufferline.nvim"
   },
-  ["cmp-buffer"] = {
+  ["coc.nvim"] = {
     loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/cmp-buffer",
-    url = "https://github.com/hrsh7th/cmp-buffer"
-  },
-  ["cmp-cmdline"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
-    url = "https://github.com/hrsh7th/cmp-cmdline"
-  },
-  ["cmp-nvim-lsp"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
-    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
-  },
-  ["cmp-path"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/cmp-path",
-    url = "https://github.com/hrsh7th/cmp-path"
-  },
-  ["cmp-vsnip"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
-    url = "https://github.com/hrsh7th/cmp-vsnip"
-  },
-  ["friendly-snippets"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/friendly-snippets",
-    url = "https://github.com/rafamadriz/friendly-snippets"
+    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    url = "https://github.com/neoclide/coc.nvim"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
@@ -124,20 +99,10 @@ _G.packer_plugins = {
     path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
-  ["nvim-cmp"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/nvim-cmp",
-    url = "https://github.com/hrsh7th/nvim-cmp"
-  },
   ["nvim-colorizer.lua"] = {
     loaded = true,
     path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/norcalli/nvim-colorizer.lua"
-  },
-  ["nvim-lspconfig"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
-    url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
@@ -174,11 +139,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
-  },
-  ["vim-vsnip"] = {
-    loaded = true,
-    path = "/Users/faisalsh/.local/share/nvim/site/pack/packer/start/vim-vsnip",
-    url = "https://github.com/hrsh7th/vim-vsnip"
   }
 }
 
